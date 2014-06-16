f698981e4efb1407edb1356ccd75bf3d59e97a80
jobro13 <jochembrouwer@telfort.nl>
2014-06-16 19:22:56 +0200

    play the game

--- rowgame.lua
+++ rowgame.lua
@@ -3,4 +3,4 @@ local rows = require "rows"
 game = rows.new()
 game:Initialize()
 
-for i = 1, 7 do game:Place(math.random(1,7)) end 
+game:Play()
--- rows.lua
+++ rows.lua
@@ -14,11 +14,15 @@ rows.Player2Color = "yellow"
 rows.WinAttribute = "underline"
 rows.Turn = "Player1"
 
+rows.Won = false
+rows.PlayerWon = nil
+
 -- z0mg hax
 rows.Animate = true 
 rows.AnimateSleep = 0.1
 
 function rows:Initialize()
+	os.execute('tput clear')
 	self.Board = {}
 	for i = 1, self.Collumns do 
 		self.Board[i] = {}
@@ -38,7 +42,7 @@ function rows:Place(collumn)
 	local Symbol = self[self.Turn]
 	local h = self:getHeight(collumn)
 	if h == self.Rows+1 then 
-		-- NOPE
+		return false 
 	else 
 		if not self.Animate then 
 			self.Board[tonumber(collumn)][tonumber(h)] = Symbol
@@ -57,9 +61,14 @@ function rows:Place(collumn)
 			end 
 		end 
 
-		self:checkForWin(collumn,h)
+		local won = self:checkForWin(collumn,h)
+		if won then 
+			self.Won = true 
+			self.PlayerWon = self.Turn
+		end
 		self:ChangeTurn()
-	end
+	end 
+	return true
 end
 
 function rows:GetState(x,y)
@@ -71,6 +80,7 @@ function rows:PrintBoardState()
 	os.execute("tput clear")
 	color("%{blackbg}")
 	color("%{white}|"..delim..delim)
+
 	for x = 1, self.Collumns do 
 		io.write((x)..delim)
 	end 
@@ -81,10 +91,16 @@ function rows:PrintBoardState()
 			--print(x.." "..y.." : ".. (self:GetState(x,y) or ""))
 
 			local posx = self:GetState(x,y)
+
+			local attr = ""
+			if posx and posx:match("%%") then 
+				attr = "blink"
+			end
+
 			if posx == self.Player1 then 
-				color("%{"..self.Player1Color.."}".. posx..delim)
+				color("%{"..self.Player1Color.." "..attr.."}".. posx..delim)
 			elseif posx == self.Player2 then 
-				color("%{"..self.Player2Color.."}"..posx..delim)
+				color("%{"..self.Player2Color.." "..attr.."}"..posx..delim)
 			else 
 				io.write(string.rep(" ", self.Player1:len()) .. delim)
 			end
@@ -95,28 +111,103 @@ function rows:PrintBoardState()
 	local lr = 5 + (self.Collumns * (1 + self.Player1:len()))
 	for i = 1, lr do io.write("-") end 
 	io.write("\n")
-	os.execute("tput rc")
+	--os.execute("tput home")
 end
 
 function rows:getHeight(collumn) -- returns row height (-1 is posy)
 	for y=1,self.Rows do 
-		print("check", collumn, y, self:GetState(collumn,y))
+
 		if not self:GetState(collumn,y) then 
-			print(y)
+
 			return y 
 		end 
 	end  
 
-	return self.Rows
+	return self.Rows+1
 end
 
 function rows:checkForWin(x,y)
+	local function fbound(sx,sy,dx,dy,rs) -- start x, start y, delta x, delta y (direction move), (reset with)
+		local symb = self:GetState(sx,sy)
+		local bsymb = symb -- backup symb to check
+		local tmpx, tmpy = sx,sy
+		while symb do 
+			tmpx = tmpx + dx 
+			tmpy = tmpy + dy 
+			if not rs then 
+				if tmpx < 1 or tmpx > self.Collumns then 
+					symb=nil 
+				elseif tmpy < 1 or tmpy > self.Rows then 
+					symb = nil 
+				else   
+					symb = self:GetState(tmpx, tmpy) == bsymb
+				end
+			else 
+				if tmpx < 1 or tmpx > self.Collumns then 
+					symb=nil 
+				elseif tmpy < 1 or tmpy > self.Rows then 
+					symb = nil 
+				else   
+					symb = self:GetState(tmpx, tmpy) == bsymb
+					if symb then 
+						self.Board[tmpx][tmpy] = bsymb.."%"
+					end
+				end
+				
+			end 
+		end
+		
+		return tmpx - dx, tmpy - dy -- to fix the bound 
+	end
+
+
+
 	-- HORIZONTAL
+	-- FIND LEFT BOUND:
+	local lx = fbound(x,y,-1,0)
+	-- FIND RIGHT BOUND
+	local rx = fbound(x,y,1,0)
+	if rx - lx >= 3 then 
+		fbound(x,y,-1,0,true)
+		fbound(x,y,1,0,true)
+		return true 
+	end
 
 	-- VERTICAL
 
+	local uy = fbound(x,y,0,1)
+	-- FIND RIGHT BOUND
+	local dy = fbound(x,y,0,-1)
+	print(uy,dy, "bound")
+	if math.abs(uy-dy) >= 3 then 
+		fbound(x,y,0,1,true)
+		fbound(x,y,0,-1,true)
+		return true 
+	end
+
 	-- SIDEWAYS
 
+	-- down;
+
+	local lx = fbound(x,y,-1,-1)
+
+	local rx = fbound(x,y,1,1)
+	if rx - lx >= 3 then 
+		fbound(x,y,-1,-1,true) -- replace
+		fbound(x,y,1,1,true)
+		return true 
+	end
+
+	-- up 
+
+	local lx = fbound(x,y,-1,1)
+	-- FIND RIGHT BOUND
+	local rx = fbound(x,y,1,-1)
+	if rx - lx >= 3 then 
+		fbound(x,y,-1,1,true)
+		fbound(x,y,1,-1,true)
+		return true 
+	end
 
 end
 
@@ -128,6 +219,26 @@ function rows:GetAvailableMoves() -- returns a table; values are available moves
 		end
 	end
 	return out
+end
+
+function rows:Play() -- YEAH :D
+	self:PrintBoardState() -- visuals yay
+	while not self.Won do 
+		print(self.Turn.."'s turn... (type a collumn number to place!)")
+		local finished = false
+		repeat 
+			local coll = io.read("*n")
+			if not coll then 
+				print("Invalid input, try again.")
+			else 
+				finished = self:Place(tonumber(coll))
+				if not finished then 
+					print("Invalid move, try again.")
+				end
+			end
+		until finished
+	end
+	print(self.PlayerWon.." has won!")
 end
 
 
